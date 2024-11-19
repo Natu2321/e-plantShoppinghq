573ead68093bcef1d452da9a59960aaba123f4ae
natu2321 <natnaellove21@gmail.com>
2024-11-19 03:24:36 -0500

    Task7

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,18 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
-import CartItem from './CartItem';
+import CartItem from './CartItem'
+import addItem from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
+    const [addedToCart, setAddedToCart] = useState({});
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
     const ProductList = () => {
     const plantsArray = [
         {
